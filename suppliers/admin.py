c0f6e65342adfc4a364963f681e3dd25215c78d2
Evgeny Safonov <jeka-luda@yandex.ru>
2024-07-20 20:22:52 +0300

    add SupplierAdmin

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from suppliers.models import Supplier
 
-# Register your models here.
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    """Отображение модели Supplier в админ-панели Django."""
+
+    list_display = ('name', 'email', 'country', 'city', 'level', 'supplier', 'debt', 'created_at')
+    list_filter = ('city',)
+    search_fields = ('name', 'city',)
+    actions = ['cleanup_debt']
+
+    def cleanup_debt(self, request, queryset):
+        """Очистка задолженности перед поставщиком у выбранных объектов."""
+
+        queryset.update(debt=0.00)
+
+    cleanup_debt.short_description = 'Очистить задолженность'
