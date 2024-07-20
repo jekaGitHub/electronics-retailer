c28286b3f4ca4492fa55d9fec2fc2d9aa4cd8117
Evgeny Safonov <jeka-luda@yandex.ru>
2024-07-20 17:56:30 +0300

    register ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from products.models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "owner",
+        "title",
+        "model",
+        "release_date",
+    )
+    list_filter = ("owner", "title",)
+    search_fields = ("title",)
