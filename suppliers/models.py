35805502f2353d576c135cb1c448c61f3a122157
Evgeny Safonov <jeka-luda@yandex.ru>
2024-07-20 18:57:33 +0300

    create model Supplier and migrate

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Supplier(models.Model):
+    """Модель поставщика торговой сети электроники."""
+
+    LEVEL_CHOICES = [
+        (0, 'Завод'),
+        (1, 'Розничная сеть'),
+        (2, 'Индивидуальный предприниматель'),
+    ]
+
+    name = models.CharField(max_length=100, verbose_name='Название')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    street = models.CharField(max_length=100, verbose_name='Улица')
+    house = models.CharField(max_length=100, verbose_name='Номер дома')
+    level = models.IntegerField(choices=LEVEL_CHOICES, verbose_name='Уровень иерархии')
+    supplier = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        verbose_name='Поставщик',
+        related_name='suppliers'
+    )
+    debt = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00,
+        verbose_name='Задолженность перед поставщиком'
+    )
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Звено сети'
+        verbose_name_plural = 'Звенья сети'
