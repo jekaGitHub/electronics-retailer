ba054cbd13e8ac06a73780ec896f8fa062388576
Evgeny Safonov <jeka-luda@yandex.ru>
2024-07-20 21:01:44 +0300

    create views for models

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """Создание продукта и автоматическое добавление владельца к продукту."""
+
+        product = serializer.save()
+        product.owner = self.request.user
+        product.save()
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from suppliers.models import Supplier
+from suppliers.serializers import SupplierSerializer
+
+
+class SupplierViewSet(ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ['country']
+
+    def perform_create(self, serializer):
+        """Создание звена торговой сети и автоматическое добавление владельца."""
+
+        serializer.save(owner=self.request.user)
